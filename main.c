bf7035ebd752e452812067eee5263124ae4cf660
Flyewzz <dentalon599@gmail.com>
2020-11-14 15:53:12 +0300

    Something hanged

--- main.c
+++ main.c
@@ -1,39 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define SIZE 20
+#define SIZE 30
+
 // Функция быстрой сортировки
-void quickSort(int *numbers, int left, int right)
+void quickSort(int *elements, int left, int right)
 {
   int pivot; // разрешающий элемент
-  int l_hold = left; //левая граница
-  int r_hold = right; // правая граница
-  pivot = numbers[left];
+  int lHold = left; //левая граница
+  int rHold = right; // правая граница
+  pivot = elements[left];
   while (left < right) // пока границы не сомкнутся
   {
-    while ((numbers[right] >= pivot) && (left < right)) {
+    while ((elements[right] >= pivot) && (left < right)) {
         right--; // сдвигаем правую границу пока элемент [right] больше [pivot]
     }
     if (left != right) // если границы не сомкнулись
     {
-      numbers[left] = numbers[right]; // перемещаем элемент [right] на место разрешающего
+      elements[left] = elements[right]; // перемещаем элемент [right] на место разрешающего
       left++; // сдвигаем левую границу вправо 
     }
-    while ((numbers[left] <= pivot) && (left < right))
+    while ((elements[left] <= pivot) && (left < right))
       left++; // сдвигаем левую границу пока элемент [left] меньше [pivot]
     if (left != right) // если границы не сомкнулись
     {
-      numbers[right] = numbers[left]; // перемещаем элемент [left] на место [right]
+      elements[right] = elements[left]; // перемещаем элемент [left] на место [right]
       right--; // сдвигаем правую границу вправо 
     }
   }
-  numbers[left] = pivot; // ставим разрешающий элемент на место
+  elements[left] = pivot; // ставим разрешающий элемент на место
   pivot = left;
-  left = l_hold;
-  right = r_hold;
+  left = lHold;
+  right = rHold;
   if (left < pivot) // Рекурсивно вызываем сортировку для левой и правой части массива
-    quickSort(numbers, left, pivot - 1);
+    quickSort(elements, left, pivot - 1);
   if (right > pivot)
-    quickSort(numbers, pivot + 1, right);
+    quickSort(elements, pivot + 1, right);
 }
 int main()
 {
